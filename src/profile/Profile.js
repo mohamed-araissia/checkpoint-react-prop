0811503945ed954efb99c0f17d795b72c887363f
mohamed-araissia <63782409+mohamed-araissia@users.noreply.github.com>
2020-09-10 11:52:12 +0200

    Update Profile.js

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import myImage from '../image1.jpg'
 import PropTypes from 'prop-types'
-export default function Profile({fullName , bio, profession }) {
-   
-    const  handleName = (e) => {
+
+
+
+export default function Profile({props}) {
+   console.log(props)
+    const handleName = (e) => {
         e.preventDefault();
-        alert(fullName);
+        alert(e);
       }
     return (
-        <div>
-        <picture>
-   <img src={myImage} alt='' width="300" height="200" onClick={handleName}/>
-        </picture>
+        <div >
+            
+            
+<picture onClick={handleName}>
+    {props.children}
+</picture>    
+
+    
         </div>
     )
 }
 Profile.defaultProps = {
-fullName:"unknwon",
-bio:"none"
+fullName:"fullname",
+bio:"bio",
+profession:"profession"   
 }
 Profile.propTypes ={
     fullName:PropTypes.string,
